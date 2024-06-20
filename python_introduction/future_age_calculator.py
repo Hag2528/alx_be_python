c52885faa92a58958d56dd7824878089ee189e2d
Hagos Chekole <Hagochek06@gmail.com>
2024-06-20 13:12:30 +0300

    age is calculted

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,3 +1,3 @@
-current_age= int(input("How old are you? :"))
+current_age= int(input("How old are you? "))
 user_age_after_2050=current_age+27
 print("In 2050, you will be",user_age_after_2050 ,"years old.")

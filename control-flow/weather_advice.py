eb138924445161a530b412b46a478953885cf742
Hagos Chekole <Hagochek06@gmail.com>
2024-06-27 00:37:00 +0300

    Done with wether

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
 current_whether=input("What's the weather like today? (sunny/rainy/cold):")
-if current_whether.lower()=="sunny":
+if current_whether=="sunny":
     print("Wear a t-shirt and sunglasses.")
-elif current_whether.lower()=="rainy":
+elif current_whether=="rainy":
     print("Don't forget your umbrella and a raincoat.")
 
-elif current_whether.lower()=="cold":
+elif current_whether=="cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")

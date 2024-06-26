9384f36096ae94d0fd94272471e64d6d6bc2e51e
Hagos Chekole <Hagochek06@gmail.com>
2024-06-27 02:47:02 +0300

    weather is modified

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,8 @@
 whether=input("What's the weather like today? (sunny/rainy/cold):")
-if(whether=="sunny"):
+if whether=="sunny":
     print("Wear a t-shirt and sunglasses.")
 elif whether=="rainy":
     print("Don't forget your umbrella and a raincoat.")
-
 elif whether=="cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:

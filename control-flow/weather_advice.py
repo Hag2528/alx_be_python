2595573c7acaa81dead535cdd9b4ae7bb2a8af9c
Hagos Chekole <Hagochek06@gmail.com>
2024-06-27 00:40:06 +0300

    done with weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-current_whether=input("What's the weather like today? (sunny/rainy/cold):")
-if current_whether=="sunny":
+whether=input("What's the weather like today? (sunny/rainy/cold):")
+if whether=="sunny":
     print("Wear a t-shirt and sunglasses.")
-elif current_whether=="rainy":
+elif whether=="rainy":
     print("Don't forget your umbrella and a raincoat.")
 
-elif current_whether=="cold":
+elif whether=="cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")

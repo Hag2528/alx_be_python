d8a109b0324eca0892d7a91285cc9552388b7a34
Hagos Chekole <Hagochek06@gmail.com>
2024-06-28 12:21:18 +0300

    daily reminder is done fine

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,18 +1,12 @@
-# Get task details
-task = input("Enter a single task: ")
-priority = input("Enter priority (high, medium, low): ")
-time_bound = input("Is this task time-bound (yes/no): ")
-
-# Process task based on priority and time sensitivity
-#reminder_message = ""
+task=input("Enter a single task:")
+priority=input("Enter priority (high,medium,low):")
+time_bound=input("Is this task time-bound (yes/no):")
 match priority:
   case "high":
     if time_bound == "yes":
       print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
     if time_bound=="no":
       print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
-
-    #reminder_message = "'High Priority Task:'" + task
   case "medium":
       if time_bound == "yes":
         print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")

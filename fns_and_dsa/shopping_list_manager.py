60a92aa3946b2847b6c46be3ccebdd61d952b49b
Hagos Chekole <Hagochek06@gmail.com>
2024-07-06 00:26:56 +0300

    shopping coded

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,12 +12,10 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            # Prompt for and add an item
             new_item = input("Enter item name: ")
             shopping_list.append(new_item)
             print(f"{new_item} added to the list.")
         elif choice == '2':
-            # Prompt for and remove an item
             item_to_remove = input("Enter item name to remove: ")
             if item_to_remove in shopping_list:
                 shopping_list.remove(item_to_remove)
@@ -25,7 +23,6 @@ def main():
             else:
                 print(f"{item_to_remove} not found in the list.")
         elif choice == '3':
-            # Display the shopping list
             if shopping_list:
                 print("Shopping List:")
                 for item in shopping_list:

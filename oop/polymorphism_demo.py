6e3a6999d12ab0698e8ad279d3241703ba1999e9
Hag2528 <hagochek53@gmail.com>
2024-07-19 18:49:55 +0300

    Update polymorphism_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -3,11 +3,11 @@ class Shape:
     def area(slef):
         raise NotImplementedError
 class Rectangle(Shape):
-      def __init__(self,lenght,width):
-          self.lenght=lenght
+      def __init__(self,length,width):
+          self.lenght=length
           self.width=width
       def area(self):
-           return  self.lenght * self.width
+           return  self.length * self.width
 class Circle(Shape):
     def __init__(self,radius):
          self.radius=radius
